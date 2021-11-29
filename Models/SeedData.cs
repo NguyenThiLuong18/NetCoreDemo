7aa33d88c09cc05637b00cb9b57e53a214b4f7d5
LUONG NGUYEN THI <1821051118@student.humg.edu.vn>
2021-11-29 15:59:57 +0700

    Validation

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -27,7 +27,7 @@ namespace NetCoreDemo.Models
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
                         Rating = "R",
-                        Price = 7.99M
+                        Price = 799M
                     },
 
                     new Movie
@@ -36,7 +36,7 @@ namespace NetCoreDemo.Models
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
                         Rating = "C",
-                        Price = 8.99M
+                        Price = 899M
                     },
 
                     new Movie
@@ -45,7 +45,7 @@ namespace NetCoreDemo.Models
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
                         Rating = "C",
-                        Price = 9.99M
+                        Price = 999M
                     },
 
                     new Movie
@@ -54,7 +54,7 @@ namespace NetCoreDemo.Models
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
                         Rating = "W",
-                        Price = 3.99M
+                        Price = 399M
                     }
                 );
                 context.SaveChanges();
