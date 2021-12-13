ea275a245d3e1734474a0bf0b35ed20c2496bed6
LUONG NGUYEN THI <1821051118@student.humg.edu.vn>
2021-12-13 16:35:57 +0700

    Upload/Excels

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +8,23 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using NetCoreDemo.Models;
 using NetCoreDemo.Data;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
 namespace NetCoreDemo.Controllers
 {
     public class MoviesController : Controller
     {
         private readonly NetCoreDbContext _context;
-
-        public MoviesController(NetCoreDbContext context)
+        private ExcelProcess _excelPro = new ExcelProcess();
+        public MoviesController(NetCoreDbContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
+        public IConfiguration Configuration {get;}
 
          // GET: Movie
         // GET: Movies
@@ -50,6 +58,67 @@ namespace NetCoreDemo.Controllers
         return View(movieGenreVM);
     }
 
+
+        public IActionResult UploadFile(){
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile file){
+            if (file!=null)
+            {
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Chọn đúng File: "+ fileExtension);
+                }
+                    else
+                {
+                    //rename file when upload to server
+                    //tao duong dan /Uploads/Excels de luu file upload len server
+                    var fileName = "Movie";
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", fileName + fileExtension);
+                    var fileLocation = new FileInfo(filePath).ToString();
+
+                    if (ModelState.IsValid)
+                    {
+                        //upload file to server
+                        if (file.Length > 0)
+                        {
+                            using (var stream = new FileStream(filePath,FileMode.Open, FileAccess.Read, FileShare.Read))
+                            {
+                                //save file to server
+                                file.CopyToAsync(stream);
+                                //read data from file and write to database
+                                //_excelPro la doi tuong xu ly file excel ExcelProcess
+                                var dt = _excelPro.ExcelToDataTable(fileLocation);
+                                //ghi du lieu datatable vao database
+                                //var sobanghithanhcong = WriteDatatableToDatabase(dt);
+                                for(int i = 0; i< dt.Rows.Count; i++){
+                                    var movies = new Movie();
+                                    movies.Id = Convert.ToInt32(dt.Rows[i][0].ToString());
+                                    movies.Title = dt.Rows[i][1].ToString();
+                                    movies.ReleaseDate = Convert.ToDateTime(dt.Rows[i][2].ToString());
+                                    movies.Price = Convert.ToDecimal(dt.Rows[i][3].ToString());
+                                    movies.Genre = dt.Rows[i][4].ToString();
+                                    movies.Rating = dt.Rows[i][5].ToString();
+                                    _context.Add(movies);
+                                }
+                                _context.SaveChanges();
+                                var sobanghithanhcong = dt.Rows.Count;
+                                ModelState.AddModelError("", "Ghi thanh cong du lieu, bao gom: " + sobanghithanhcong + " ban ghi");
+                                
+                            }
+                            //return RedirectToAction(nameof(UploadFile));
+                        }
+                    }
+                }
+                
+                
+                
+            }
+                return View();
+        }
+
         
 
         // GET: Movies/Details/5
@@ -73,6 +142,52 @@ namespace NetCoreDemo.Controllers
         // GET: Movies/Create
         public IActionResult Create()
         {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IFormFile file)
+        {
+            
+            if (file!=null)
+            {
+              
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+                else
+                {
+                    //rename file when upload to server
+                    //tao duong dan /Uploads/Excels de luu file upload len server
+                    var fileName = "Movie";
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", fileName + fileExtension);
+                    var fileLocation = new FileInfo(filePath).ToString();
+
+                    if (ModelState.IsValid)
+                    {
+                        //upload file to server
+                        if (file.Length > 0)
+                        {
+                            using (var stream = new FileStream(filePath,FileMode.Open, FileAccess.Read, FileShare.Read))
+                            {
+                                //save file to server
+                                await file.CopyToAsync(stream);
+                                //read data from file and write to database
+                                //_excelPro la doi tuong xu ly file excel ExcelProcess
+                                var dt = _excelPro.ExcelToDataTable(fileLocation);
+                                //ghi du lieu datatable vao database
+                                
+                            }
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
+            }
+            else {
+                 ModelState.AddModelError("", "Chọn đúng file!");
+            }
             return View();
         }
 
@@ -176,5 +291,23 @@ namespace NetCoreDemo.Controllers
         {
             return _context.Movie.Any(e => e.Id == id);
         }
+        // private int WriteDatatableToDatabase (DataTable dt){
+        //    // try{
+        //         var con = Configuration.GetConnectionString("NetCoreDbContext");
+        //         SqlBulkCopy bulkcopy = new SqlBulkCopy (con);
+        //         bulkcopy.DestinationTableName = "Movies";
+        //         bulkcopy.ColumnMappings.Add(0, "Title");
+        //         bulkcopy.ColumnMappings.Add(1, "ReleaseDate");
+        //         bulkcopy.ColumnMappings.Add(2, "Price");
+        //         bulkcopy.ColumnMappings.Add(3, "Genre");
+        //         bulkcopy.ColumnMappings.Add(4, "Rating");
+        //         bulkcopy.WriteToServer (dt);
+        //     // }
+        //     // catch{
+        //     //     return 0;
+        //     // }
+        //     return dt.Rows.Count;
+        // }
     }
+    
 }
